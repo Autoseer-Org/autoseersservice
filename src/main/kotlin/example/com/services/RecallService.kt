578e065473c980e0036c148ea91bf1360d7f07fe
David Acosta <dacostacsit@gmail.com>
2024-08-11 19:22:43 -0400

    Update imports for recallservice

--- src/main/kotlin/example/com/services/RecallService.kt
+++ src/main/kotlin/example/com/services/RecallService.kt
@@ -1,14 +1,17 @@
 package example.com.services
 
-import example.com.models.GeminiSummaryModel
 import example.com.models.PublicRecallResponse
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.append
 import io.ktor.http.headers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.internal.NopCollector.emit
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
